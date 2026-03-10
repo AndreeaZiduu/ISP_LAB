6845b1127cfba5ccd07ff4410b7e495a87b97059
AndreeaZiduu <andreeamaria.zidu@ulbsibiu.ro>
2026-03-10 17:40:50 +0200

    Reparare foldere laboratoare

---
+++ TLab1/src/Main.java
@@ -0,0 +1,7 @@
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+void main(String[] args) {
+    Student student1 = new Student(300, "Mihaela", "Popescu", "ISM Grupa 21");
+    System.out.println(student1);
+
+}
---
+++ TLab1/src/Student.java
@@ -0,0 +1,18 @@
+public class Student {
+    private int numarMatricol;
+    private String prenume;
+    private String nume;
+    private String formatieDeStudiu;
+
+    public Student (int numarMatricol,String prenume, String nume, String formatieDeStudiu) {
+        this.numarMatricol=numarMatricol;
+        this.prenume=prenume;
+        this.nume=nume;
+        this.formatieDeStudiu=formatieDeStudiu;
+    }
+    public String toString(){
+        return "Student: " + numarMatricol+ ", "+  prenume +", "+ nume + ", " + formatieDeStudiu;
+    }
+
+}
+
