21ec97547202587c97df5efee4284159e89f3813
AndreeaZiduu <andreeamaria.zidu@ulbsibiu.ro>
2026-03-15 15:46:17 +0200

    ex 2, punctul a

--- TLab1/src/Main.java
+++ TLab1/src/Main.java
@@ -1,7 +1,25 @@
+import java.util.ArrayList;
+import java.util.List;
+
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
-void main(String[] args) {
-    Student student1 = new Student(300, "Mihaela", "Popescu", "ISM Grupa 21");
-    System.out.println(student1);
+public class Main {
+    public static void main(String[] args) {
+        Student student1 = new Student(300, "Mihaela", "Popescu", "ISM Grupa 21");
+        System.out.println(student1);
+        Student student2 = new Student(210, "Bianca", "Florescu", "ISM Grupa 21");
+        Student student3 = new Student(90, "Maria", "Zidu", "ISM Grupa 21");
+        Student student4 = new Student(157, "Ana", "Berea", "ISM Grupa 21");
 
+        List<Student> ListaStudenti = new ArrayList();
+        ListaStudenti.add(student1);
+        ListaStudenti.add(student2);
+        ListaStudenti.add(student3);
+        ListaStudenti.add(student4);
+
+        for (Student s : ListaStudenti)
+            System.out.println(s);
+
+
+    }
 }
